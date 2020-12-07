484b552741aeccc15ed35fabff53d9cbed74f20f
Ben Spotswood <spotswoodb@gmail.com>
2020-12-07 16:52:18 -0600

    commit tree 87e157284d298768901a085ad53e5fc1b1a8e2a9

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,19 @@
 class Cat
+ attr_accessor :mood, :owner
+  attr_reader :name
+ 
+ @@all = []
+
+ 
   # code goes here
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
